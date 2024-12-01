5100c56ebfa5f8edad80b8bb9ed8c3f06ae6441f
AdamLesage <adamles44@gmail.com>
2024-12-01 18:02:55 +0100

    feat(Auth system): add resgister and login methods with google correct redirection

--- backend/routes/authentication.js
+++ backend/routes/authentication.js
@@ -10,6 +10,70 @@ const express = require('express');
 // const prisma = new PrismaClient();
 const router = express.Router();
 const passport = require('passport');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const bcrypt = require('bcrypt');
+const { v4: uuidv4 } = require('uuid');
+
+// Intern auth routes
+
+router.get('/login', (req, res) => {
+    const { password, email } = req.body;
+
+    if (!password || !email) {
+        return res.status(400).json({ error: 'Missing parameters' });
+    }
+
+    prisma.user.findUnique({
+        where: {
+            email,
+        },
+    }).then((user) => {
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        bcrypt.compare(password, user.password, (err, result) => {
+            if (result) {
+                return res.status(200).json({ message: 'User logged in' });
+            }
+            return res.status(401).json({ error: 'Invalid password' });
+        });
+    });
+});
+
+router.post('/register', async (req, res) => {
+    const { password, email, name, surname, bio, birthDate, phoneNumber, profilePicture } = req.body;
+
+    if (!password || !email || !name || !surname) {
+        return res.status(400).json({ error: 'Missing required parameters' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const uuid = uuidv4();
+
+    prisma.user.create({
+        data: {
+            email,
+            hashedPassword,
+            name,
+            surname,
+            bio,
+            uuid: uuid,
+            birthDate: birthDate ? new Date(birthDate) : null,
+            phoneNumber,
+            profilePicture: profilePicture ? { create: profilePicture } : undefined,
+        },
+    }).then((user) => {
+        return res.status(201).json(user);
+    }).catch((error) => {
+        console.error(error);
+        return res.status(500).json({ error: error.message });
+    });
+});
+
+
+// Google auth routes
 
 router.get('/google',
     passport.authenticate('google', {
@@ -17,19 +81,50 @@ router.get('/google',
         prompt: 'select_account',
     })
 );
-
 router.get('/google/redirect',
     passport.authenticate('google', {
         failureRedirect: '/login'
     }),
     (req, res) => {
         console.log('User:', req.user);
-        res.redirect('/');
+        const userParams = {
+            email: req.user.emails[0].value,
+            name: req.user.name.givenName,
+            surname: req.user.name.familyName || '',
+            // profilePicture: {
+            //     create: {
+            //         url: req.user.photos[0].value,
+            //     },
+            // },
+            uuid: uuidv4(),
+            googleAccessToken: req.user.accessToken,
+            hashedPassword: '',
+        };
+
+        prisma.user.findUnique({
+            where: {
+                email: userParams.email,
+            },
+        }).then((user) => {
+            if (!user) {
+                prisma.user.create({
+                    data: userParams,
+                }).then((newUser) => {
+                    return res.status(201).json(newUser);
+                }).catch((error) => {
+                    console.error(error);
+                    return res.status(500).json({ error: error.message });
+                });
+            } else {
+                return res.status(200).json(user);
+            }
+        }).catch((error) => {
+            console.error(error);
+            return res.status(500).json({ error: error.message });
+        });
     }
 );
 
-router.get('/login', (req, res) => {
-    res.send('Login page');
-});
-
 module.exports = router;
+
+'\nInvalid `prisma.user.create()` invocation in\n/home/alesage/Epitech/tek3/area/area-epitech/backend/routes/authentication.js:106:29\n\n  103     },\n  104 }).then((user) => {\n  105     if (!user) {\n→ 106         prisma.user.create({\n                data: {\n                  email: "area.romainlemalin@gmail.com",\n                  name: "Area google account",\n                  surname: "",\n                  profilePicture: "https://lh3.googleusercontent.com/a/ACg8ocLLl2qUAgTmvvCgFnEEkdNMs6MSMa_5bJTmqiC2CkMaGARRYA=s96-c",\n                                  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n                  uuid: "e2068a38-8647-474c-8d48-cc96addcd2dc",\n                  googleAccessToken: undefined,\n                  hashedPassword: ""\n                }\n              })\n\nArgument `profilePicture`: Invalid value provided. Expected ProfilePictureCreateNestedOneWithoutUserInput or ProfilePictureUncheckedCreateNestedOneWithoutUserInput, provided String.'
