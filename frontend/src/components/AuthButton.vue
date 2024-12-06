2c32ff4e454f3583c1c274cb12249581b80b9559
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-12-06 17:46:27 +0100

    add(AuthButton) : Added a small component that takes an icon and a color for the icon as parameter.

---
+++ frontend/src/components/AuthButton.vue
@@ -0,0 +1,16 @@
+<template>
+    <button
+        class="bg-white w-[6.75rem] h-[3.25rem] rounded-lg border-2 border-auth-neutral flex justify-center items-center">
+        <Icon :icon="icon" class="w-8 h-8" :style="{ color: color }" />
+    </button>
+</template>
+
+<script setup lang="ts">
+import { Icon } from '@iconify/vue';
+import { defineProps } from 'vue';
+
+defineProps<{
+    color: string,
+    icon: string
+}>()
+</script>
