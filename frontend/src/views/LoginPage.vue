2aad92f32a6ff62377a358c05945a1918b6696df
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-12-06 15:07:37 +0100

    add(LoginPage) : Add the login page with the necessary calls to each child components.

---
+++ frontend/src/views/LoginPage.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import LoginFormComponent from '@/components/LoginFormComponent.vue';
+import LogoComponent from '@/components/LogoComponent.vue';
+import SignUpButton from '@/components/SignUpButton.vue';
+
+import { ref } from 'vue';
+
+const signupHover = ref(false);
+</script>
+
+<template>
+    <div class="bg-auth-primary w-20 h-20 hover:cursor-pointer">
+        <LogoComponent color="#80C4E9" class="hover:cursor-pointer"/>
+        <LoginFormComponent />
+        <SignUpButton
+            class="hover:cursor-pointer"
+            :color="signupHover ? 'black' : 'white'"
+            @mouseover="signupHover = true"
+            @mouseleave="signupHover = false" />
+    </div>
+</template>
+
+<style scoped>
+</style>
