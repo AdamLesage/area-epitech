a425169fd2b04699550b506f36b450cb98eeabea
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-12-06 15:05:37 +0100

    add(Login route) : Added the login route path and page inside the router config.

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,9 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LoginPage from '@/views/LoginPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     // Here you put the different routes of the application
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginPage,
+    },
   ],
 })
 
