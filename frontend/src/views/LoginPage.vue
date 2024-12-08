40fad3fefbe475a9aecda30d045f1ea07604d323
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-12-08 11:38:03 +0100

    add(Mobile support) : Started working on the mobile app support on the Login Page.

--- frontend/src/views/LoginPage.vue
+++ frontend/src/views/LoginPage.vue
@@ -4,18 +4,26 @@ import LoginFormComponent from '@/components/LoginFormComponent.vue';
 import SignUpButton from '@/components/SignUpButton.vue';
 import { LoginFormValues } from '@/types/auth';
 
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
+import { Capacitor } from '@capacitor/core';
 
+const isMobile = ref(true);
 const hover = ref(false);
 
 // Form submission handler
 const handleSubmit = (values: LoginFormValues) => {
     console.log('Login Form Received:', values);
 };
+
+onMounted(() => {
+    if (Capacitor.getPlatform() == 'web')
+        isMobile.value = false;
+});
 </script>
 
 <template>
-    <div class="bg-auth-primary flex justify-center items-center">
+    <div class="bg-auth-primary flex justify-center items-center"
+        v-if="!isMobile">
         <LogoComponent color="#80C4E9" class="absolute top-0 left-5" />
         <LoginFormComponent @submit="handleSubmit" />
         <SignUpButton
@@ -23,6 +31,11 @@ const handleSubmit = (values: LoginFormValues) => {
             :color="hover ? '#eee' : 'white'"
             @mouseover="hover = true"
             @mouseleave="hover = false" />
+    </div>
+    <div class="bg-auth-primary flex justify-center items-center"
+        v-else>
+        <h1 class="text-3xl text-white">Login</h1>
+        <h2 class="text-xl text-white">Mobile Login Coming Soon</h2>
     </div>
 </template>
 
