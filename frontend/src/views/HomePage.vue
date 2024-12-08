0234891d3daf0be179bec87aac32092d832e0550
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-12-08 16:39:40 +0100

    add(Missing links) : Added missing links to the home page for testing purposes.

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -27,7 +27,9 @@ onMounted(() => {
         <nav class="flex gap-5 flex-col">
             <router-link to="/login" class="hover:cursor-pointer" >Login</router-link>
             <router-link to="/signup" class="hover:cursor-pointer" >Sign Up</router-link>
+            <router-link to="/signup/user-details" class="hover:cursor-pointer" >User Details</router-link>
             <router-link to="/forgot-password" class="hover:cursor-pointer" >Forgot Password</router-link>
+            <router-link to="/change-password" class="hover:cursor-pointer" >Change Password</router-link>
             <router-link to="/email-verification" class="hover:cursor-pointer" >Email Verification</router-link>
         </nav>
     </div>
