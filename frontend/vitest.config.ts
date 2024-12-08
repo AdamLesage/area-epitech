abef089e3d7b4609375ac530dba95b5acf31a9e4
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-12-08 11:39:15 +0100

    add (Compilation options) : Added build dir option and noEmit param for vue-tsc

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -10,5 +10,9 @@ export default mergeConfig(
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
     },
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true,
+    }
   }),
 )
