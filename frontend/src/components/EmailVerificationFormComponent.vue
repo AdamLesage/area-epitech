68698246f1dcf216371efd1019e9a4575ee4a9d5
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-12-08 17:24:26 +0100

    add (Mobile Version) : Added the mobile version of the email verification page.

--- frontend/src/components/EmailVerificationFormComponent.vue
+++ frontend/src/components/EmailVerificationFormComponent.vue
@@ -1,62 +1,66 @@
 <template>
-    <div class="bg-white w-[32rem] pt-6 pb-2 px-4 rounded-[1.5rem] gap-2 flex flex-col">
-        <header class="mb-6">
-            <h1 class="text-center text-3xl font-bold text-auth-primary">Email Verification</h1>
-            <h2 class="text-center text-auth-primary">We have sent a 6 digits code to your email.</h2>
-        </header>
-        <Form
-            class="flex flex-col gap-4 px-4"
-            :validation-schema="schema"
-            @submit="onSubmit"
-            :initial-values="initialValues">
-            <!-- Code Field -->
-            <div class="flex flex-col justify-center items-center space-x-2">
-                <div class="flex justify-between">
-                    <Field
-                        name="code"
-                        type="text"
-                        v-model="code"
-                        maxlength="6">
-                        <input
-                            v-for="(_, index) in 6"
-                            :key="index"
-                            v-model="code[index]"
+    <div class="web:bg-white web:w-[32rem] mobile:w-[22rem] web:py-6 mobile:pb-6 mobile:h-full px-4 rounded-[1.5rem] web:gap-8 mobile:gap-4 flex flex-col justify-start web:min-h-[20rem]">
+        <div class="flex flex-col justify-between h-full">
+            <header class="flex flex-col gap-4 mb-4">
+                <h1 class="text-center text-3xl font-bold web:text-auth-primary mobile:text-white">Email Verification</h1>
+                <h2 class="text-center web:text-auth-primary mobile:text-white">We have sent a 6 digits code to your email.</h2>
+            </header>
+            <Form
+                class="flex flex-col px-4 h-5/6 justify-between gap-4"
+                :validation-schema="schema"
+                @submit="onSubmit"
+                :initial-values="initialValues">
+                <!-- Code Field -->
+                <div class="flex flex-col justify-center items-center space-x-2">
+                    <div class="flex justify-between">
+                        <Field
+                            name="code"
                             type="text"
-                            class="w-[15%] h-16 text-center border-2 border-auth-neutral placeholder:text-auth-neutral rounded-md"
-                            maxlength="1"
-                            @input="handleInput(index)"
-                            @keydown.backspace="handleBackspace(index, $event)" />
-                    </Field>
+                            v-model="code"
+                            maxlength="6">
+                            <input
+                                v-for="(_, index) in 6"
+                                :key="index"
+                                v-model="code[index]"
+                                type="text"
+                                class="w-[15%] h-16 text-center border-2 border-auth-neutral placeholder:text-auth-neutral rounded-md"
+                                maxlength="1"
+                                @input="handleInput(index)"
+                                @keydown.backspace="handleBackspace(index, $event)" />
+                        </Field>
+                    </div>
+                    <ErrorMessage name="code" class="text-red-500 text-sm mt-1 text-center" />
                 </div>
-                <ErrorMessage name="code" class="text-red-500 text-sm mt-1 text-center" />
-            </div>
-            <!-- Email Recap Field : disabled -->
-            <div class="flex flex-col">
-                <label for="email" class="text-auth-primary mb-2">Your email:</label>
-                <Field
-                    name="email"
-                    type="email"
-                    :value="props.email"
-                    placeholder="No email provided..."
-                    class="p-2 border-2 border-auth-neutral placeholder:text-auth-neutral text-auth-neutral rounded-lg w-full cursor-not-allowed"
-                    disabled />
-                <ErrorMessage name="email" class="text-red-500 text-sm mt-1" />
-            </div>
-            <!-- Validate Button -->
-            <h2 class="text-center text-auth-primary mb-8 mt-8">Not received? <span class="font-bold hover:cursor-pointer hover:underline" @click="sendAgain">Send Again</span></h2>
-            <button
-                type="submit"
-                class="bg-auth-primary text-white p-2 rounded-lg hover:cursor-pointer">
-                Validate
-            </button>
-        </Form>
-        <!-- Abort Button -->
-        <button
-            type="button"
-            class="text-auth-primary p-2 rounded-lg hover:cursor-pointer mx-4"
-            @click="abort">
-            Abort
-        </button>
+                <!-- Email Recap Field : disabled -->
+                <div class="flex flex-col">
+                    <label for="email" class="web:text-auth-primary mobile:text-white mb-2">Your email:</label>
+                    <Field
+                        name="email"
+                        type="email"
+                        :value="props.email"
+                        placeholder="No email provided..."
+                        class="p-2 border-2 border-auth-neutral placeholder:text-auth-neutral text-auth-neutral rounded-lg w-full cursor-not-allowed bg-white"
+                        disabled />
+                    <ErrorMessage name="email" class="text-red-500 text-sm mt-1" />
+                </div>
+                <div class="flex flex-col gap-2">
+                    <!-- Validate Button -->
+                    <h2 class="text-center web:text-auth-primary mobile:text-white mb-8 mt-8">Not received? <span class="font-bold hover:cursor-pointer hover:underline" @click="sendAgain">Send Again</span></h2>
+                    <button
+                        type="submit"
+                        class="web:bg-auth-primary mobile:bg-auth-tertiary text-white p-2 rounded-lg hover:cursor-pointer">
+                        Validate
+                    </button>
+                    <!-- Abort Button -->
+                    <button
+                        type="button"
+                        class="web:text-auth-primary mobile:text-white p-2 rounded-lg hover:cursor-pointer mx-4"
+                        @click="abort">
+                        Abort
+                    </button>
+                </div>
+            </Form>
+        </div>
     </div>
 </template>
 
--- frontend/src/views/EmailVerificationPage.vue
+++ frontend/src/views/EmailVerificationPage.vue
@@ -21,12 +21,15 @@ const handleSendAgain = () => {
 
 <template>
     <div class="bg-auth-primary flex justify-center items-center">
-        <LogoComponent color="#80C4E9" class="absolute top-0 left-5" />
-        <EmailVerificationFormComponent
-            email="test@gmail.com"
-            @submit="handleSubmit"
-            @abort="handleAbort"
-            @send-again="handleSendAgain" />
+        <LogoComponent color="#80C4E9" class="absolute top-0 left-5 half:hidden" />
+        <div class="flex flex-col items-center z-10 mobile:justify-between web:justify-center h-full">
+            <h1 class="text-[4rem] leading-[3rem] font-black text-auth-secondary web:hidden my-4">AREA</h1>
+            <EmailVerificationFormComponent
+                email="test@gmail.com"
+                @submit="handleSubmit"
+                @abort="handleAbort"
+                @send-again="handleSendAgain" />
+        </div>
     </div>
 </template>
 
