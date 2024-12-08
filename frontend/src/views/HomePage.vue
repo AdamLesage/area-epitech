1b53866cf4ac4cb1a56b770921096a2622d0d22e
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-12-08 11:36:34 +0100

    add(HomePage) : Added a simple home page to enable navigation on mobile.

---
+++ frontend/src/views/HomePage.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { Capacitor } from '@capacitor/core';
+import { onMounted, ref } from 'vue';
+
+const isMobile = ref(true);
+
+onMounted(() => {
+    if (Capacitor.getPlatform() == 'web')
+        isMobile.value = false;
+});
+</script>
+
+<template>
+    <div class="bg-auth-primary"
+        v-if="!isMobile">
+        <h1 class="text-3xl text-white text-center">Web Home</h1>
+        <nav class="flex gap-5">
+            <router-link to="/login" class="hover:cursor-pointer" >Login</router-link>
+            <router-link to="/signup" class="hover:cursor-pointer" >Sign Up</router-link>
+            <router-link to="/forgot-password" class="hover:cursor-pointer" >Forgot Password</router-link>
+            <router-link to="/email-verification" class="hover:cursor-pointer" >Email Verification</router-link>
+        </nav>
+    </div>
+    <div class="bg-auth-primary"
+        v-else>
+        <h1 class="text-3xl text-white text-center">Mobile Home</h1>
+        <nav class="flex gap-5 flex-col">
+            <router-link to="/login" class="hover:cursor-pointer" >Login</router-link>
+            <router-link to="/signup" class="hover:cursor-pointer" >Sign Up</router-link>
+            <router-link to="/forgot-password" class="hover:cursor-pointer" >Forgot Password</router-link>
+            <router-link to="/email-verification" class="hover:cursor-pointer" >Email Verification</router-link>
+        </nav>
+    </div>
+</template>
