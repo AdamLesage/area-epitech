a1ea5dfbabe64d632e61d12903adb2055854cb1d
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-12-08 16:40:09 +0100

    add (Responsive design) : Added responsive design on the signup user details page.

--- frontend/src/views/UserDetailsPage.vue
+++ frontend/src/views/UserDetailsPage.vue
@@ -16,8 +16,11 @@ const handleSkip = () => {
 
 <template>
     <div class="bg-auth-primary flex justify-center items-center">
-        <LogoComponent color="#80C4E9" class="absolute top-0 left-5" />
-        <DetailsFormComponent @submit="handleSubmit" @skip="handleSkip" />
+        <LogoComponent color="#80C4E9" class="absolute top-0 left-5 half:hidden" />
+        <div class="flex flex-col items-center z-10 mobile:justify-between web:justify-center h-full">
+            <h1 class="text-[4rem] leading-[3rem] font-black text-auth-secondary web:hidden my-4">AREA</h1>
+            <DetailsFormComponent @submit="handleSubmit" @skip="handleSkip" />
+        </div>
     </div>
 </template>
 
