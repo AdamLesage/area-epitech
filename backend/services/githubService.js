8128bf563ca02b674cd7cf9a653f4c203d905c8a
AdamLesage <adamles44@gmail.com>
2024-12-09 13:11:31 +0100

    feat(Service): github webhook create new reaction in database

--- backend/services/githubService.js
+++ backend/services/githubService.js
@@ -1,16 +1,48 @@
 const express = require('express');
 const router = express.Router();
+const Prisma = require('@prisma/client');
+const prisma = new Prisma.PrismaClient;
 
-router.post('/webhook', (req, res) => {
-    console.log('Requête reçue :');
-    const event = req.headers['x-github-event'];
-    const action = req.body['action'];
-    const eventSender = req.body.sender['login'];
+router.post('/webhook', async (req, res) => {
+    try {
+        const event = req.headers['x-github-event'];
+        const action = req.body['action'];
+        const eventSender = req.body.sender?.login;
 
-    console.log(`New event: ${event} ${action} receive from ${eventSender}`)
+        console.log(`New event: ${event} ${action} received from ${eventSender}`);
 
-    // Réponse pour confirmer que la requête est reçue
-    res.status(200).send('Webhook reçu avec succès');
+        // Check if service exists
+        let service = await prismaClient.service.findUnique({
+            where: { name: 'Github' },
+        });
+
+        if (!service) {
+            service = await prismaClient.service.create({
+                data: {
+                    name: 'Github',
+                    logo: './frontend/src/assets/github_logo.png', // Need to put correct path for the asset
+                },
+            });
+        }
+
+        // Create new reaction
+        const reaction = await prismaClient.reaction.create({
+            data: {
+                name: `Reaction for ${event} ${action}`,
+                description: `Triggered by ${event} event and ${action} action`,
+                endpoint: `/api/reactions/github/${event}/${action}`,
+                service: {
+                    connect: { id: service.id },
+                },
+            },
+        });
+
+        console.log('New reaction created :', reaction);
+
+        res.status(200).send('Webhook received and processed successfully');
+    } catch (error) {
+        res.status(500).send('Internal server error');
+    }
 });
 
 module.exports = router;
