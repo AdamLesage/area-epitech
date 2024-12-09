d8346c15cc78babed1fd48309afc883d5e61580e
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-12-09 22:21:27 +0100

    rework (Store data) : Used store data instead of hardcoded data which makes each service unique.

--- frontend/src/views/ServicePage.vue
+++ frontend/src/views/ServicePage.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import { useRoute } from 'vue-router';
-import { AreaValues } from '@/types/services';
+import { useRoute, useRouter } from 'vue-router';
 import { Icon } from '@iconify/vue';
+import { useServiceStore } from '@/stores/service';
 
 import ServiceNavComponent from '@/components/ServiceNavComponent.vue';
 import ServiceNavScrollComponent from '@/components/ServiceNavScrollComponent.vue';
@@ -12,88 +12,41 @@ import ArrowComponent from '@/components/ArrowComponent.vue';
 import AREAInfoComponent from '@/components/AREAInfoComponent.vue';
 
 const route = useRoute();
+const router = useRouter();
 
-const serviceId = route.params.id;
-const color = ref<string>('#24292E');
-const name = ref<string>('Github');
-const logo = ref<string>('mdi:github');
-const rate = ref<number>(3.4);
-const reviews = ref<number>(10000);
-const saves = ref<number>(50);
+const serviceId: string = Array.isArray(route.params.id) ? route.params.id[0] : route.params.id;
+
+const store = useServiceStore();
+console.log('Service ID:', serviceId);
+const service = store.services.find(service => service.name === serviceId);
+console.log('Service:', service);
+
+if (service === undefined) {
+    console.error('Service not found');
+    router.push('/dashboard');
+}
+
+const color = ref<string>(service!.color);
+const name = ref<string>(service!.name);
+const logo = ref<string>(service!.icon);
+const rate = ref<number>(service!.reviews.rate);
+const reviews = ref<number>(service!.reviews.count);
+const saves = ref<number>(service!.saves);
 const isActivated = ref<boolean>(true);
-const nbActions = ref<number>(5);
-const nbTriggers = ref<number>(3);
+const nbActions = ref<number>(0);
+
+if (service?.actions) {
+    nbActions.value = service.actions.length;
+}
+
+const nbTriggers = ref<number>(0);
+
+if (service?.reactions) {
+    nbTriggers.value = service.reactions.length;
+}
+
 const nameCapitalized = ref(name.value.toUpperCase());
 const isHeroVisible = ref(true);
-
-const areas = ref<AreaValues[]>([
-    {
-        title: 'On Mail Received',
-        subtitle: 'When you receive an email',
-        description: 'This trigger gets called when you receive an email in your mailbox.',
-        status: true,
-    },
-    {
-        title: 'On New Tweet',
-        subtitle: 'When a new tweet is posted',
-        description: 'This trigger gets called when a new tweet is posted by a user.',
-        status: false,
-    },
-    {
-        title: 'On New Tweet',
-        subtitle: 'When a new tweet is posted',
-        description: 'This trigger gets called when a new tweet is posted by a user.',
-        status: false,
-    },
-    {
-        title: 'On New Tweet',
-        subtitle: 'When a new tweet is posted',
-        description: 'This trigger gets called when a new tweet is posted by a user.',
-        status: false,
-    },
-    {
-        title: 'On New Tweet',
-        subtitle: 'When a new tweet is posted',
-        description: 'This trigger gets called when a new tweet is posted by a user.',
-        status: false,
-    },
-    {
-        title: 'On New Tweet',
-        subtitle: 'When a new tweet is posted',
-        description: 'This trigger gets called when a new tweet is posted by a user.',
-        status: false,
-    },
-    {
-        title: 'On New Tweet',
-        subtitle: 'When a new tweet is posted',
-        description: 'This trigger gets called when a new tweet is posted by a user.',
-        status: false,
-    },
-    {
-        title: 'On New Tweet',
-        subtitle: 'When a new tweet is posted',
-        description: 'This trigger gets called when a new tweet is posted by a user.',
-        status: false,
-    },
-    {
-        title: 'On New Tweet',
-        subtitle: 'When a new tweet is posted',
-        description: 'This trigger gets called when a new tweet is posted by a user.',
-        status: false,
-    },
-    {
-        title: 'On New Tweet',
-        subtitle: 'When a new tweet is posted',
-        description: 'This trigger gets called when a new tweet is posted by a user.',
-        status: false,
-    },
-    {
-        title: 'On New Tweet',
-        subtitle: 'When a new tweet is posted',
-        description: 'This trigger gets called when a new tweet is posted by a user.',
-        status: false,
-    }
-])
 
 const isCircleFirst = ref(true);
 const scrollY = ref(0);
@@ -117,16 +70,33 @@ const handleBackButtonSecondPage = () => {
     isHeroVisible.value = true;
 }
 
-function handleMenuClick(area: AreaValues) {
-    console.log('Menu clicked on:', area.title);
+function handleMenuClick(name: string) {
+    console.log('Menu clicked on:', name);
 }
 
-function handleMoreClick(area: AreaValues) {
-    console.log('More clicked on:', area.title);
+function handleMoreClick(name: string) {
+    console.log('More clicked on:', name);
 }
 
-function handleConfigureClick(area: AreaValues) {
-    console.log('Configure clicked on:', area.title);
+function handleConfigureClick(name: string) {
+    console.log('Configure clicked on:', name);
+}
+
+function handleBackButtonFirstPage() {
+    console.log('Back button clicked on first page');
+    router.push('/dashboard');
+}
+
+function handleScrollAttempt(event: WheelEvent) {
+    if (event.deltaY > 0) {
+        openServicePage();
+    }
+}
+
+function handleScrollAttemptSecondPage(event: WheelEvent) {
+    if (event.deltaY < 0 && scrollY.value === 0) {
+        isHeroVisible.value = true;
+    }
 }
 </script>
 
@@ -136,8 +106,9 @@ function handleConfigureClick(area: AreaValues) {
         :style="{ backgroundColor: color }"
         class="flex flex-col justify-between"
         @click="openServicePage"
+        @wheel="handleScrollAttempt"
         v-if="isHeroVisible">
-        <ServiceNavComponent />
+        <ServiceNavComponent @back-button="handleBackButtonFirstPage" />
         <div class="flex flex-col justify-center items-center">
             <div class="flex justify-center items-center p-4">
                 <Icon :icon="logo" class="w-36 h-36 text-white" />
@@ -146,10 +117,10 @@ function handleConfigureClick(area: AreaValues) {
                     <h2 class="text-white text-xl font-medium text-right w-full pr-2">{{ nbTriggers }} Triggers / {{ nbActions }} Actions</h2>
                 </div>
             </div>
-            <div 
+            <div
                 class="border-4 border-auth-neutral w-[300px] h-[90px] rounded-full bg-white flex justify-between items-center px-4 cursor-pointer transition-transform duration-300"
                 @click.stop="handleClick">
-                <div 
+                <div
                     v-if="isCircleFirst" 
                     class="rounded-full w-[60px] h-[60px] transition-all duration-500" 
                     :style="{ backgroundColor: color }" 
@@ -160,7 +131,7 @@ function handleConfigureClick(area: AreaValues) {
                 >
                     {{ isCircleFirst ? 'Activate' : 'Deactivate' }}
                 </h1>
-                <div 
+                <div
                     v-if="!isCircleFirst"
                     class="rounded-full w-[60px] h-[60px] transition-all duration-500" 
                     :style="{ backgroundColor: color }" 
@@ -174,7 +145,9 @@ function handleConfigureClick(area: AreaValues) {
         </div>
     </div>
     <!-- Second Page -->
-    <div v-else>
+    <div
+        @wheel="handleScrollAttemptSecondPage"
+        v-else>
         <div class="flex flex-col items-center justify-between h-1/2"
             :style="{ backgroundColor: color }">
             <ServiceNavComponent @back-button="handleBackButtonSecondPage" />
@@ -219,16 +192,26 @@ function handleConfigureClick(area: AreaValues) {
             </div>
             <SaveComponent :saves="saves" :color="color" textcolor="black" class="w-1/3 flex justify-end" />
         </div>
-        <div class="flex flex-wrap gap-8 p-8 justify-center">
+        <div class="flex flex-wrap gap-8 p-8 justify-center"
+            v-if="service && service.actions">
             <AREAInfoComponent
-                v-for="area in areas"
-                :key="area.title"
-                :values="area"
+                v-for="action in service.actions"
+                :key="action.name"
+                :values="action"
                 :icon="logo"
                 :color="color"
-                @menu-click="handleMenuClick(area)"
-                @more-click="handleMoreClick(area)"
-                @configure-click="handleConfigureClick(area)" />
+                @menu-click="handleMenuClick(action.name)"
+                @more-click="handleMoreClick(action.name)"
+                @configure-click="handleConfigureClick(action.name)" />
+            <AREAInfoComponent
+                v-for="reaction in service.reactions"
+                :key="reaction.name"
+                :values="reaction"
+                :icon="logo"
+                :color="color"
+                @menu-click="handleMenuClick(reaction.name)"
+                @more-click="handleMoreClick(reaction.name)"
+                @configure-click="handleConfigureClick(reaction.name)" />
         </div>
     </div>
 </template>
