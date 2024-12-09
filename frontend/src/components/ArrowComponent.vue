25b686ad630258d22a953fb973468e4f1668566d
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-12-09 18:42:25 +0100

    add (Arrow Component) : Added an arrow component used in the service page.

---
+++ frontend/src/components/ArrowComponent.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue';
+
+defineProps<{
+    color: string,
+}>()
+</script>
+
+<template>
+    <Icon icon="solar:arrow-down-bold-duotone" class="w-6 h-6 animation-up-down" :style="{ color: color }" />
+</template>
+
+<style scoped>
+@keyframes upDown {
+    0%, 100% {
+        transform: translateY(0);
+    }
+    50% {
+        transform: translateY(-10px);
+    }
+}
+
+.animation-up-down {
+    animation: upDown 1.5s infinite;
+}
+</style>
