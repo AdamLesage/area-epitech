d8313e4358b491a5f1dc692a57f515300e385a8e
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-12-09 18:11:32 +0100

    add (Cors) : Added cors setup in abckend app.js.

--- backend/app.js
+++ backend/app.js
@@ -7,10 +7,12 @@ require('./authentication/passport');
 const userRouter = require('./routes/user');
 const authRouter = require('./routes/authentication');
 const aboutRouter = require('./routes/about')
+const cors = require('cors');
 
 const app = express();
 const port = 8080;
 
+app.use(cors());
 app.use(express.json());
 app.use(
   session({
