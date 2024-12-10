e7588d5ff9a47955fea1b28e7e3ca7f217fbdb43
mathieu-MZ <mathieu.mazeau@epitech.eu>
2024-12-10 18:13:47 +0100

    feat (Front) add the button to create area and a recuparation of users service for the my area page

--- frontend/src/views/MyAreaPage.vue
+++ frontend/src/views/MyAreaPage.vue
@@ -1,3 +1,109 @@
+<template>
+    <!-- First Page -->
+    <div :style="{ backgroundColor: color }" class="flex flex-col justify-between" @click="openServicePage"
+        @wheel="handleScrollAttempt" v-if="isHeroVisible">
+        <ServiceNavComponent @back-button="handleBackButtonFirstPage" @redirect-user-profile="handleRedirectUserPage"
+            class="mobile:hidden" />
+        <RateComponent :rate="rate" :reviews="reviews" color="white" textcolor="white" class="web:hidden p-4" />
+        <div class="flex flex-col justify-center items-center">
+            <div class="flex justify-center items-center p-4 web:flex-row mobile:flex-col">
+                <Icon :icon="logo" class="w-36 h-36 text-white" />
+                <div class="flex flex-col justify-end items-center p-4">
+                    <h1
+                        class="text-white web:text-[6rem] mobile:text-[3rem] web:leading-[5rem] mobile:leading-[2.5rem] font-bold">
+                        {{ nameCapitalized }}</h1>
+                    <h2 class="text-white text-xl font-medium text-right w-full pr-2 mobile:hidden">{{ nbActions }}
+                        Actions</h2>
+                </div>
+            </div>
+
+        </div>
+        <div class="flex flex-col web:hidden gap-2">
+            <h2 class="text-white text-xl font-bold text-center w-full pr-2">{{ nbActions }} Actions</h2>
+            <div class="flex flex-row gap-2 items-center justify-center">
+                <button class="rounded-full py-2 px-6 bg-white w-32">
+                    <h1 class="font-semibold">Activate</h1>
+                </button>
+                <Icon icon="material-symbols:bookmark-outline" class="w-8 h-8 text-white hover:cursor-pointer" />
+            </div>
+        </div>
+        <div class="flex justify-between items-center p-4 mobile:hidden">
+            <RateComponent :rate="rate" :reviews="reviews" color="white" textcolor="white" />
+            <ArrowComponent color="white" class="mobile:hidden" />
+            <SaveComponent :saves="saves" color="white" textcolor="white" />
+        </div>
+        <MobileServiceNavComponent @back-button="handleBackButtonFirstPage" class="web:hidden" />
+    </div>
+    <!-- Second Page -->
+    <div @wheel="handleScrollAttemptSecondPage" v-else>
+        <div class="flex flex-col items-center justify-between web:h-1/2 mobile:h-full"
+            :style="{ backgroundColor: color }">
+            <ServiceNavComponent @back-button="handleBackButtonSecondPage"
+                @redirect-user-profile="handleRedirectUserPage" class="mobile:hidden" />
+            <div class="flex justify-center items-center p-4 mobile:hidden" v-if="scrollY == 0">
+                <Icon :icon="logo" class="w-36 h-36 text-white" />
+                <div class="flex flex-col justify-end items-center p-4">
+                    <h1 class="text-white text-[6rem] leading-[5rem] font-bold">{{ nameCapitalized }}</h1>
+                    <h2 class="text-white text-xl font-medium text-right w-full pr-2">{{ nbActions }} Actions</h2>
+                </div>
+            </div>
+            <div class="fixed top-0 flex justify-center items-center w-full mobile:hidden"
+                :style="{ backgroundColor: color }" v-else>
+                <ServiceNavScrollComponent @back-button="handleBackButtonSecondPage" :logo="logo"
+                    :title="nameCapitalized" />
+            </div>
+            <div class="flex justify-between items-center w-full web:hidden">
+                <div class="flex web:justify-center items-center p-4 w-full mobile:justify-start">
+                    <Icon :icon="logo" class="web:w-36 mobile:w-[48px] web:h-36 mobile:h-[48px] text-white" />
+                    <div class="flex flex-col justify-end items-center web:p-4 mobile:p-2">
+                        <h1
+                            class="text-white web:text-[6rem] mobile:text-[2rem] web:leading-[5rem] mobile:leading-[1.75rem] font-bold">
+                            {{ nameCapitalized }}</h1>
+                        <h2 class="text-white text-xl font-medium text-right w-full pr-2 mobile:hidden">{{ nbActions }}
+                            Actions</h2>
+                    </div>
+                </div>
+                <Icon icon="material-symbols:bookmark-outline" class="w-8 h-8 text-white hover:cursor-pointer mr-4" />
+            </div>
+            <div class="flex w-full justify-center web:hidden m-4">
+                <button class="rounded-full py-2 px-6 bg-white w-32">
+                    <h1 class="font-semibold">Activate</h1>
+                </button>
+            </div>
+            <div class="flex flex-col items-center justify-start w-full p-4 web:hidden gap-4 overflow-y-scroll h-1/2 mx-auto"
+                @wheel.stop>
+                <div class="flex justify-center w-full">
+                    <button @click="handleCreateButtonClick" class="rounded-full py-2 px-6 bg-blue-500 text-white w-32">
+                        <h1 class="font-semibold">Create Area</h1>
+                    </button>
+                </div>
+                <AREACreationComponent v-if="showCreationComponent" @close="handleCloseCreationComponent" />
+                <AREAInfoComponent v-for="action in actions" :key="action.name" :values="action" :icon="action.icon"
+                    :color="action.color" @menu-click="handleMenuClick(action.name)"
+                    @more-click="handleMoreClick(action.name)" @configure-click="handleConfigureClick(action.name)" />
+            </div>
+            <div class="mobile:hidden" />
+            <MobileServiceNavComponent @back-button="handleBackButtonSecondPage" class="web:hidden" />
+        </div>
+        <div class="flex justify-between items-center p-8 mobile:hidden">
+            <RateComponent :rate="rate" :reviews="reviews" :color="color" textcolor="black" class="w-1/3" />
+            <SaveComponent :saves="saves" :color="color" textcolor="black" class="w-1/3 flex justify-end" />
+        </div>
+        <button @click="handleCreateButtonClick" class="rounded-full py-2 px-6 bg-blue-500 text-white w-32 ml-4">
+                <h1 class="font-semibold">Create Area</h1>
+            </button>
+            <AREACreationComponent v-if="showCreationComponent" @close="handleCloseCreationComponent" />
+        <div class="flex flex-wrap gap-8 p-8 justify-center mobile:hidden">
+
+
+            <AREAInfoComponent v-for="action in actions" :key="action.name" :values="action" :icon="action.icon"
+                :color="action.color" @menu-click="handleMenuClick(action.name)"
+                @more-click="handleMoreClick(action.name)" @configure-click="handleConfigureClick(action.name)" />
+        </div>
+    </div>
+</template>
+
+
 <script setup lang="ts">
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
@@ -10,6 +116,7 @@ import RateComponent from '@/components/RateComponent.vue';
 import SaveComponent from '@/components/SaveComponent.vue';
 import ArrowComponent from '@/components/ArrowComponent.vue';
 import AREAInfoComponent from '@/components/AREAInfoComponent.vue';
+import AREACreationComponent from '@/components/AREACreationComponent.vue';
 
 const router = useRouter();
 
@@ -87,115 +194,14 @@ const actions = ref([
     { name: 'Spotify track to github issue', description: 'Create a new issue on a repository when a new track is added to a playlist', icon: 'mdi:spotify', color: '#22C55E' },
     { name: 'Google calendar event to slack', description: 'Send a message on slack when a new event is created on a calendar', icon: 'mdi:slack', color: '#8A2BE2' },
 ]);
-</script>
-<template>
-    <!-- First Page -->
-    <div
-        :style="{ backgroundColor: color }"
-        class="flex flex-col justify-between"
-        @click="openServicePage"
-        @wheel="handleScrollAttempt"
-        v-if="isHeroVisible">
-        <ServiceNavComponent
-            @back-button="handleBackButtonFirstPage"
-            @redirect-user-profile="handleRedirectUserPage"
-            class="mobile:hidden" />
-        <RateComponent :rate="rate" :reviews="reviews" color="white" textcolor="white" class="web:hidden p-4" />
-        <div class="flex flex-col justify-center items-center">
-            <div class="flex justify-center items-center p-4 web:flex-row mobile:flex-col">
-                <Icon :icon="logo" class="w-36 h-36 text-white" />
-                <div class="flex flex-col justify-end items-center p-4">
-                    <h1 class="text-white web:text-[6rem] mobile:text-[3rem] web:leading-[5rem] mobile:leading-[2.5rem] font-bold">{{ nameCapitalized }}</h1>
-                    <h2 class="text-white text-xl font-medium text-right w-full pr-2 mobile:hidden">{{ nbActions }} Actions</h2>
-                </div>
-            </div>
 
-        </div>
-        <div class="flex flex-col web:hidden gap-2">
-            <h2 class="text-white text-xl font-bold text-center w-full pr-2">{{ nbActions }} Actions</h2>
-            <div class="flex flex-row gap-2 items-center justify-center">
-                <button class="rounded-full py-2 px-6 bg-white w-32">
-                    <h1 class="font-semibold">Activate</h1>
-                </button>
-                <Icon icon="material-symbols:bookmark-outline" class="w-8 h-8 text-white hover:cursor-pointer" />
-            </div>
-        </div>
-        <div class="flex justify-between items-center p-4 mobile:hidden">
-            <RateComponent :rate="rate" :reviews="reviews" color="white" textcolor="white" />
-            <ArrowComponent color="white" class="mobile:hidden" />
-            <SaveComponent :saves="saves" color="white" textcolor="white" />
-        </div>
-        <MobileServiceNavComponent @back-button="handleBackButtonFirstPage" class="web:hidden" />
-    </div>
-    <!-- Second Page -->
-    <div
-        @wheel="handleScrollAttemptSecondPage"
-        v-else>
-        <div class="flex flex-col items-center justify-between web:h-1/2 mobile:h-full"
-            :style="{ backgroundColor: color }">
-            <ServiceNavComponent
-                @back-button="handleBackButtonSecondPage"
-                @redirect-user-profile="handleRedirectUserPage"
-                class="mobile:hidden" />
-            <div class="flex justify-center items-center p-4 mobile:hidden" v-if="scrollY == 0">
-                <Icon :icon="logo" class="w-36 h-36 text-white" />
-                <div class="flex flex-col justify-end items-center p-4">
-                    <h1 class="text-white text-[6rem] leading-[5rem] font-bold">{{ nameCapitalized }}</h1>
-                    <h2 class="text-white text-xl font-medium text-right w-full pr-2">{{ nbActions }} Actions</h2>
-                </div>
-            </div>
-            <div class="fixed top-0 flex justify-center items-center w-full mobile:hidden"
-                :style="{ backgroundColor: color }"
-                v-else>
-                <ServiceNavScrollComponent @back-button="handleBackButtonSecondPage"
-                    :logo="logo" :title="nameCapitalized" />
-            </div>
-            <div class="flex justify-between items-center w-full web:hidden">
-                <div class="flex web:justify-center items-center p-4 w-full mobile:justify-start">
-                    <Icon :icon="logo" class="web:w-36 mobile:w-[48px] web:h-36 mobile:h-[48px] text-white" />
-                    <div class="flex flex-col justify-end items-center web:p-4 mobile:p-2">
-                        <h1 class="text-white web:text-[6rem] mobile:text-[2rem] web:leading-[5rem] mobile:leading-[1.75rem] font-bold">{{ nameCapitalized }}</h1>
-                        <h2 class="text-white text-xl font-medium text-right w-full pr-2 mobile:hidden">{{ nbActions }} Actions</h2>
-                    </div>
-                </div>
-                <Icon icon="material-symbols:bookmark-outline" class="w-8 h-8 text-white hover:cursor-pointer mr-4" />
-            </div>
-            <div class="flex w-full justify-center web:hidden m-4">
-                <button class="rounded-full py-2 px-6 bg-white w-32">
-                    <h1 class="font-semibold">Activate</h1>
-                </button>
-            </div>
-            <div class="flex flex-col items-center justify-start w-full p-4 web:hidden gap-4 overflow-y-scroll"
-                @wheel.stop>
-                <!-- Example AREAInfoComponent usage -->
-                <AREAInfoComponent
-                    v-for="action in actions"
-                    :key="action.name"
-                    :values="action"
-                    :icon="action.icon"
-                    :color="action.color"
-                    @menu-click="handleMenuClick(action.name)"
-                    @more-click="handleMoreClick(action.name)"
-                    @configure-click="handleConfigureClick(action.name)" />
-            </div>
-            <div class="mobile:hidden" />
-            <MobileServiceNavComponent @back-button="handleBackButtonSecondPage" class="web:hidden" />
-        </div>
-        <div class="flex justify-between items-center p-8 mobile:hidden">
-            <RateComponent :rate="rate" :reviews="reviews" :color="color" textcolor="black" class="w-1/3" />
-            <SaveComponent :saves="saves" :color="color" textcolor="black" class="w-1/3 flex justify-end" />
-        </div>
-        <div class="flex flex-wrap gap-8 p-8 justify-center mobile:hidden">
-            <!-- Example AREAInfoComponent usage -->
-            <AREAInfoComponent
-                v-for="action in actions"
-                :key="action.name"
-                :values="action"
-                :icon="action.icon"
-                :color="action.color"
-                @menu-click="handleMenuClick(action.name)"
-                @more-click="handleMoreClick(action.name)"
-                @configure-click="handleConfigureClick(action.name)" />
-        </div>
-    </div>
-</template>
+const showCreationComponent = ref(false);
+
+const handleCreateButtonClick = () => {
+    showCreationComponent.value = true;
+}
+
+const handleCloseCreationComponent = () => {
+    showCreationComponent.value = false;
+}
+</script>
