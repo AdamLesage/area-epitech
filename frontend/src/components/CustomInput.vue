044acf760314feddd9661a588bd3e5095cc8551f
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-12-28 11:11:30 +0100

    add (TextArea support) : Added support for the textarea type.

--- frontend/src/components/CustomInput.vue
+++ frontend/src/components/CustomInput.vue
@@ -1,27 +1,42 @@
 <template>
     <div>
-        <input v-if="isInput" :type="props.type" :placeholder="props.name" class="border-2 border-[#777] px-3 py-1 rounded-md text-[#333]"
+        <input v-if="isSupported && isDefaultInput" :type="props.type" :placeholder="props.name" :value="value" class="border-2 border-[#777] px-3 py-1 rounded-md text-[#333]"
             @change="handleChange"/>
-        <div v-else>"{{ props.type }}" type is not yet supported by CustomInput component</div>
+        <textarea v-if="isSupported && !isDefaultInput" :placeholder="props.name" :value="value" class="border-2 border-[#777] px-3 py-1 rounded-md text-[#333] max-h-36"
+            @change="handleChange"></textarea>
+        <div v-if="!isSupported">"{{ props.type }}" type is not yet supported by CustomInput component</div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import { OptionType } from '@/types/services';
 
 const props = defineProps<{
     name: string;
     type: OptionType;
+    value: string | undefined;
 }>();
+
+const value = ref<string>(props.value == undefined ? '' : props.value);
+
+watch(() => props.value, (newValue) => {
+    value.value = newValue == undefined ? '' : newValue;
+});
 
 const emit = defineEmits(['change']);
 
-const isInput = ref(false);
+const isSupported = ref(false);
+const isDefaultInput = ref(false);
+
 if (props.type == 'text' || props.type == 'number' || props.type == 'email' || props.type == 'password'
     || props.type == 'date' || props.type == 'time' || props.type == 'month' || props.type == 'week'
     || props.type == 'file' || props.type == 'checkbox' || props.type == 'radio') {
-    isInput.value = true;
+    isSupported.value = true;
+    isDefaultInput.value = true;
+}
+if (props.type == 'textarea') {
+    isSupported.value = true;
 }
 
 function handleChange(event: Event) {
