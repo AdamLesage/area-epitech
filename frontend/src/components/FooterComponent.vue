dd434d08bcf01058f26545a93f69482f3c9320c7
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-12-28 11:11:57 +0100

    fix (Title Opacity) : Modified title opacity from 0.7 to 0.9.

--- frontend/src/components/FooterComponent.vue
+++ frontend/src/components/FooterComponent.vue
@@ -7,7 +7,9 @@
                 <p class="text-white hover:cursor-pointer" @click="copyEmail">contact.area.ownspace@gmail.com</p>
             </div>
             <p class="w-full text-center text-white">Project made under Epitech © PGE program</p>
-            <h1 class="text-4xl font-black text-white/60 text-center w-full hover:cursor-pointer" @click="scrollToTop">AREA</h1>
+            <div class="w-full flex justify-center">
+                <h1 class="text-4xl font-black text-white text-center hover:cursor-pointer" @click="scrollToTop">AREA</h1>
+            </div>
         </div>
         <div class="flex justify-center items-center gap-8 mt-8 text-white/60 text-sm">
             <p class="hover:underline hover:cursor-pointer">Mentions</p>
