a547bf2097cae267898847ec911ec03a305c4459
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-12-31 15:06:48 +0100

    rework (Reworked HomePage) : Added a new how it works segment and a background grid.

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -25,6 +25,7 @@ const scrollY = ref(0);
 const serviceStore = useServiceStore();
 const userReview = ref('');
 const userRate = ref(1);
+const showDetails = ref(false);
 
 function sendReview() {
     console.log('Review:', userReview.value, 'Rate:', userRate.value);
@@ -58,6 +59,13 @@ const scrollToServices = () => {
     const services = document.getElementById('services');
     if (services) {
         services.scrollIntoView({ behavior: 'smooth' });
+    }
+};
+
+const scrollToHowItWorks = () => {
+    const howItWorks = document.getElementById('how-it-works');
+    if (howItWorks) {
+        howItWorks.scrollIntoView({ behavior: 'smooth' });
     }
 };
 
@@ -106,18 +114,19 @@ const availableServices = ref<Service[]>([])
 const serviceIdx = ref(0);
 
 const computedServices = computed(() => {
-    const numServicesToShow = 5;
+    const numServicesToShow = Math.min(3, availableServices.value.length); // Dynamically adjust if fewer services
     const totalServices = availableServices.value.length;
     const serviceIdxVal = Math.abs(serviceIdx.value);
     
     if (totalServices === 0) return [];
 
     const startIdx = (serviceIdxVal + totalServices) % totalServices;
-
     const services = [];
+
     for (let i = 0; i < numServicesToShow; i++) {
         services.push(availableServices.value[(startIdx + i) % totalServices]);
     }
+    
     return services;
 });
 
@@ -171,16 +180,20 @@ onMounted(async() => {
 </script>
 
 <template>
-    <div class="bg-home !h-full">
+    <div class="bg-home !h-full before:bg-[url('@/assets/img/Grid2.png')] before:absolute before:w-[200%] before:h-[200%] before:top-[-50%] before:left-[-50%] overflow-hidden relative before:z-0 before:rotate-[5deg] before:opacity-5">
         <!-- Signup and login button -->
+        <button class="fixed bottom-4 right-4 z-[100] w-12 h-12 bg-home-div text-white rounded-full hover:cursor-pointer hover:bg-home-hover" v-if="scrollY != 0">
+            <ArrowComponentTop color="#4C4CDC" class="text-center" @click="scrollToTop" :animate="false" />
+        </button>
         <nav class="flex items-center w-full justify-between px-8 py-4 fixed bg-home z-10">
-            <h1 class="w-full text-3xl font-black tracking-wide mb-4 md:mb-0 cursor-pointer text-home-text" @click="scrollToTop">AREA</h1>
+            <h1 class="w-1/2 text-3xl font-black tracking-wide mb-4 md:mb-0 cursor-pointer text-home-text" @click="scrollToTop">AREA</h1>
             <div class="w-full flex gap-12 items-center justify-center text-home-text">
-                <h1 class="text-xl font-bold tracking-wide mb-4 md:mb-0 cursor-pointer" @click="scrollToAboutUs">About us</h1>
-                <h1 class="text-xl font-bold tracking-wide mb-4 md:mb-0 cursor-pointer" @click="scrollToServices">Services</h1>
-                <h1 class="text-xl font-bold tracking-wide mb-4 md:mb-0 cursor-pointer" @click="scrollToReviews">Reviews</h1>
+                <h1 class="text-xl font-bold tracking-wide mb-4 md:mb-0 cursor-pointer hover:underline decoration-2" @click="scrollToAboutUs">About us</h1>
+                <h1 class="text-xl font-bold tracking-wide mb-4 md:mb-0 cursor-pointer hover:underline decoration-2" @click="scrollToHowItWorks">How it Works</h1>
+                <h1 class="text-xl font-bold tracking-wide mb-4 md:mb-0 cursor-pointer hover:underline decoration-2" @click="scrollToServices">Services</h1>
+                <h1 class="text-xl font-bold tracking-wide mb-4 md:mb-0 cursor-pointer hover:underline decoration-2" @click="scrollToReviews">Reviews</h1>
             </div>
-            <div class="w-full flex justify-end gap-8">
+            <div class="w-1/2 flex justify-end gap-8">
                 <LoginButtonText
                     class="hover:cursor-pointer"
                     color="#4C4CDC"
@@ -194,7 +207,7 @@ onMounted(async() => {
 
         <!-- Area logo -->
         <div class="flex flex-col items-center justify-center h-[100vh]">
-            <LogoComponent color="#4C4CDC" class="w-80 h-80 mb-10" />
+            <LogoComponent color="#4C4CDC" class="w-80 h-80 mb-10 z-[2]" />
             <Icon icon="material-symbols:copyright-outline" class="absolute text-home-text w-6 h-6 mt-[-18rem] mr-[-19rem] z-0" />
         </div>
         <div class="fixed w-full flex justify-center items-center">
@@ -219,49 +232,103 @@ onMounted(async() => {
                 </div>
                 <IconsComponent class="pt-24 mb-[-4rem]"/>
             </section>
-            <section class="mt-8 w-full flex flex-col items-center justify-center">
+            <section class="mt-12 mb-4 w-full flex flex-col items-center justify-center">
                 <TimelineComponent />
             </section>
             <!-- Who's -->
-            <section class="w-full flex justify-center flex-col gap-8 items-center mt-8">
+            <section class="w-full flex justify-center flex-col gap-8 items-center mt-16">
                 <div class="flex flex-wrap w-11/12 gap-8 justify-center">
                     <!-- Team Cards -->
                     <div
-                        class="bg-home-div w-1/4 rounded-2xl p-6 text-center shadow-lg transform transition duration-500 hover:cursor-pointer">
+                        class="bg-home-div w-1/4 rounded-2xl p-6 text-center shadow-lg transform transition duration-500">
                         <h3 class="font-bold text-home-text-light text-xl">Adam Lesage</h3>
                         <p class="text-sm text-gray-300 mt-2">Scrum Master<br>Ensures the project stays on track and
                             meets its goals.</p>
                     </div>
 
                     <div
-                        class="bg-home-div w-1/4 rounded-2xl p-6 text-center shadow-lg transform transition duration-500 hover:cursor-pointer">
+                        class="bg-home-div w-1/4 rounded-2xl p-6 text-center shadow-lg transform transition duration-500">
                         <h3 class="font-bold text-home-text-light text-xl">Mathieu Mazeau</h3>
                         <p class="text-sm text-gray-300 mt-2">Front Developer and Designer<br>Creates and optimizes the front-end
                             experience.</p>
                     </div>
 
                     <div
-                        class="bg-home-div w-1/4 rounded-2xl p-6 text-center shadow-lg transform transition duration-500 hover:cursor-pointer">
+                        class="bg-home-div w-1/4 rounded-2xl p-6 text-center shadow-lg transform transition duration-500">
                         <h3 class="font-bold text-home-text-light text-xl">Romain Chevallier</h3>
                         <p class="text-sm text-gray-300 mt-2">DevOps Engineer<br>Handles backend systems and automation.
                         </p>
                     </div>
 
                     <div
-                        class="bg-home-div w-1/4 rounded-2xl p-6 text-center shadow-lg transform transition duration-500 hover:cursor-pointer">
+                        class="bg-home-div w-1/4 rounded-2xl p-6 text-center shadow-lg transform transition duration-500">
                         <h3 class="font-bold text-home-text-light text-xl">Victor Hritsea</h3>
                         <p class="text-sm text-gray-300 mt-2">Backend Developer<br>Builds and manages the API backend.
                         </p>
                     </div>
 
                     <div
-                        class="bg-home-div w-1/4 rounded-2xl p-6 text-center shadow-lg transform transition duration-500 hover:cursor-pointer">
+                        class="bg-home-div w-1/4 rounded-2xl p-6 text-center shadow-lg transform transition duration-500">
                         <h3 class="font-bold text-home-text-light text-xl">Tugdual de Reviers</h3>
                         <p class="text-sm text-gray-300 mt-2">Front Developer<br>Creates and optimizes the front-end
                             experience.</p>
                     </div>
                 </div>
             </section>
+        </div>
+
+        <!-- How It Works -->
+        <div id="how-it-works" class="flex flex-col items-center">
+            <h2 class="text-2xl font-bold text-home-text mb-8 mt-8 pt-24">
+                <span class="text-home-light hover:cursor-pointer" @click="scrollToHowItWorks">#</span> HOW IT WORKS
+            </h2>
+            <div class="flex gap-8 flex-wrap justify-center w-full max-w-5xl text-center">
+                <div class="flex flex-col items-center w-1/3">
+                    <Icon icon="majesticons:hand-pointer-event-line" class="text-4xl w-16 h-16 mb-4 text-home-light" aria-hidden="true" />
+                    <h3 class="font-bold text-home-text-light text-lg">Choose an Action</h3>
+                    <p class="text-sm text-home-text-light">
+                        Select an event from your favorite service. For example, "New song added to Spotify playlist."
+                    </p>
+                </div>
+                <div class="flex flex-col items-center w-1/3">
+                    <Icon icon="icon-park-outline:robot-two" class="text-4xl w-16 h-16 mb-4 text-home-light" aria-hidden="true" />
+                    <h3 class="font-bold text-home-text-light text-lg">Define a Reaction</h3>
+                    <p class="text-sm text-home-text-light">
+                        Set the automated task you want to happen. For example, "Save song details to Dropbox."
+                    </p>
+                </div>
+                <div class="flex flex-col items-center w-1/3">
+                    <Icon icon="material-symbols:chair" class="text-4xl w-16 h-16 mb-4 text-home-light" aria-hidden="true" />
+                    <h3 class="font-bold text-home-text-light text-lg">Let US Work</h3>
+                    <p class="text-sm text-home-text-light">
+                        Sit back and relax as our app automates your workflows seamlessly.
+                    </p>
+                </div>
+            </div>
+            <button
+                class="mt-8 w-48 h-12 bg-home-div text-white rounded-lg hover:cursor-pointer hover:bg-home-hover border-2 border-solid border-home-light z-[2]"
+                @click="showDetails = !showDetails">
+                {{ showDetails ? 'Hide Details' : 'Learn More' }}
+            </button>
+
+            <!-- Detailed Explanation Div -->
+            <transition name="fade">
+                <div v-if="showDetails" class="mt-16 p-6 bg-home-div text-white rounded-lg max-w-4xl shadow-lg z-[2]">
+                    <h3 class="text-xl font-bold mb-4 text-home-text">Detailed Explanation</h3>
+                    <p class="text-md leading-6 text-home-text-light">
+                        AREA operates by connecting various online services and enabling users to automate workflows
+                        effortlessly. Here's how you can make the most of it:
+                    </p>
+                    <ul class="list-disc list-inside mt-4 text-md text-home-text-light">
+                        <li><strong>Actions:</strong> Actions are events like a new file upload, an email receipt, or a new Spotify song.</li>
+                        <li><strong>Reactions:</strong> Reactions are tasks our app executes when an action occurs, such as sending an email or saving data.</li>
+                        <li><strong>AREAs:</strong> Combine actions and reactions from multiple services to create automation workflows.</li>
+                    </ul>
+                    <p class="mt-4 text-md text-home-text hover:cursor-pointer" @click="scrollToServices">
+                        Explore all the services below for more examples and infos.
+                    </p>
+                </div>
+            </transition>
         </div>
 
         <div id="services" class="flex flex-col items-center">
@@ -271,23 +338,25 @@ onMounted(async() => {
                     <ArrowComponentLeft :animate="false" color="#4C4CDC" class="text-center hover:cursor-pointer" @click="serviceIdx--"/>
                 </div>
                 <div
-                    v-for="(service) in computedServices.slice(0, 3)"
+                    v-for="(service) in computedServices"
                     :key="service.name"
-                    class="flex flex-col items-center p-4 rounded-md w-[20rem] h-48 justify-between"
+                    class="flex flex-col items-center p-4 rounded-md w-[20rem] h-48 justify-between z-[2]"
                     :style="{ backgroundColor: service.color }"
                     :aria-label="`Explore ${service.name}`"
                     role="button"
                     tabindex="0"
                     @click="handleServiceClick(service.name)">
                     <Icon :icon="service.icon" class="text-4xl w-16 h-16 mb-2 text-white" aria-hidden="true" />
-                    <span class="text-lg font-medium text-white">{{ service.name }}</span>
+                    <span class="text-lg font-medium text-white">{{ service.name.charAt(0).toUpperCase() + service.name.slice(1) }}</span>
+                    <p class="text-sm text-white">Automate tasks with {{ service.name.charAt(0).toUpperCase() + service.name.slice(1) }} effortlessly!</p>
                     <RateComponent :rate="service.reviews.rate" :reviews="service.reviews.count" textcolor="white" color="white" />
                 </div>
                 <div class="flex items-center h-48 justify-center">
                     <ArrowComponentRight :animate="false" color="#4C4CDC" class="text-center hover:cursor-pointer" @click="serviceIdx++"/>
                 </div>
             </div>
-            <button class="w-48 h-12 bg-home-div text-white rounded-lg mt-8 hover:cursor-pointer hover:bg-home-hover border-2 border-solid border-home-light" @click="navigateTo('/')">Explore all services</button>
+            <button class="w-48 h-12 bg-home-div text-white rounded-lg mt-8 hover:cursor-pointer hover:bg-home-hover border-2 border-solid border-home-light z-[2]"
+                @click="navigateTo('/')">Explore all services</button>
         </div>
 
         <div id="reviews" class="flex flex-col items-center justify-center mt-12 mb-80">
@@ -303,7 +372,7 @@ onMounted(async() => {
                     </p>
                 </div>
                 <div
-                    class="flex flex-col items-center p-4 gap-4 rounded-lg justify-between bg-home-div w-full mt-32">
+                    class="flex flex-col items-center p-4 gap-4 rounded-lg justify-between bg-home-div w-full mt-32 z-[2]">
                     <div class="flex w-full items-center gap-4">
                         <UserSvgComponent color="#373799" />
                         <h2 class="text-home-text-light pr-4">What do you think of our website ?</h2>
@@ -330,7 +399,7 @@ onMounted(async() => {
             </section>
         </div>
 
-        <footer class="flex justify-between items-center flex-col h-64 bg-home-div py-8">
+        <footer class="flex justify-between items-center flex-col h-64 bg-home-div py-8 relative z-[2]">
             <h1 class="text-3xl font-black text-home-text-light text-center mb-8">CONTACT US</h1>
             <div class="flex w-full justify-center items-center px-8">
                 <div class="flex gap-4 items-center w-full justify-center">
@@ -351,4 +420,10 @@ onMounted(async() => {
 </template>
 
 <style scoped>
+.fade-enter-active, .fade-leave-active {
+    transition: opacity 0.3s ease;
+}
+.fade-enter-from, .fade-leave-to {
+    opacity: 0;
+}
 </style>
