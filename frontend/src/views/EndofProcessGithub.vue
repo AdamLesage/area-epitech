9ee0c3458aa5f08627a9f5f378e0aec0c7c44d52
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2025-01-09 14:24:35 +0100

    add (Redirect url for Discord Bot) : Added a simple close page for the redirection of the discord bot.

---
+++ frontend/src/views/EndofProcessGithub.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="bg-home !h-full relative overflow-hidden">
+        <div class="absolute inset-0 bg-[url('@/assets/img/Grid2.png')] w-full h-full bg-repeat bg-center opacity-5 z-0"></div>
+
+        <!-- Processing Content -->
+        <div class="flex flex-col items-center justify-center h-[100vh] z-[2]">
+            <h1 class="text-5xl font-extrabold text-white mb-4">Successfully added discord bot.</h1>
+            <p class="text-xl text-home-text-light text-center mb-2 bg-home">
+                You can now close this window!
+            </p>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
