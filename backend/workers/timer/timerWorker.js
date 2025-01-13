819cdaad58596b217e0d094639851a95ef67893c
Victor Hristea <victorhristea@gmail.com>
2025-01-13 18:07:01 +0100

    feat (backend) add the first version of the worker for the timer NOT WORKING

---
+++ backend/workers/timer/timerWorker.js
@@ -0,0 +1,44 @@
+
+const Redis = require('ioredis');
+const redis = new Redis({
+    host: 'redis',
+    port: 6379
+});
+
+async function processWebhook() {
+    while (true) {
+        const data = await redis.rpop(process.env.UUID);
+        if (data) {
+            const webhookData = JSON.parse(data);
+            // Wait for the specified timer duration before sending the data
+            await new Promise(resolve => setTimeout(resolve, process.env.TIMER_DURATION));
+            send(webhookData);
+        } else {
+            // Wait for a while before checking the queue again
+            await new Promise(resolve => setTimeout(resolve, 1000));
+        }
+    }
+}
+
+processWebhook().catch(console.error);
+
+async function send(data2) {
+    console.log("send");
+    try {
+        const response = await fetch(process.env.CALL_BACK, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data2)
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        // const data = await response.json();
+        // console.log(data);
+    } catch (error) {
+        console.error('Fetch error:', error);
+    }
+}
