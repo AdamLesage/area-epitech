16565a522e1f33776d6c0f042b59824e9642b5b8
AdamLesage <adamles44@gmail.com>
2025-01-13 15:53:39 +0100

    feat(Strava service): add backend actions for strava

--- backend/routes/reaction.js
+++ backend/routes/reaction.js
@@ -45,6 +45,7 @@ function useActionReturn(returnAction, reactionData) {
  */
 router.post('/reaction/:uuid', async (req, res) => {
     try {
+        console.log("Called reaction");
         const uuid = req.params.uuid;
         const area = await prisma.actionReaction.findUnique({where: {uuid: uuid}});
         if (area == null) {
