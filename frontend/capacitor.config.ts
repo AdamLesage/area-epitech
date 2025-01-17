452a3de8cbbf01076d0b12ce88bc190b3716992e
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2025-01-17 14:03:51 +0100

    add (App splash and icon) : Added splash screen and icon for the app.

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.arearomainlemalin.area',
-  appName: 'area-mobile',
+  appName: 'AREA',
   webDir: 'dist',
   loggingBehavior: 'none'
 };
