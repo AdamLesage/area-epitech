583d8c38626a2569725d244ed8f4f731df85cb57
AdamLesage <adamles44@gmail.com>
2025-01-18 18:58:16 +0100

    tests(Playwright tests): signIn and delete account functions for testing

--- frontend/e2e/tests/userShouldSignUp.spec.ts
+++ frontend/e2e/tests/userShouldSignUp.spec.ts
@@ -1,4 +1,5 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
+import { utilsTestingFunctions } from './utilsTestingFunctions.js';
 
 test.use({
     viewport: {
@@ -10,28 +11,6 @@ test.use({
 let code = '';
 
 test('test', async ({ page }) => {
-    await page.goto('http://area-workspace.fr:8081/#/');
-    await page.getByLabel('signup-text-button-router-link').click();
-    await page.getByPlaceholder('Enter your email...').click();
-    await page.getByPlaceholder('Enter your email...').fill('bidule@gmail.com');
-    await page.locator('input[name="password"]').click();
-    await page.locator('input[name="password"]').fill('lalalalallala');
-    await page.locator('input[name="confirmPassword"]').click();
-    await page.locator('input[name="confirmPassword"]').fill('lalalalallala');
-    await page.getByLabel('I agree to the Terms').check();
-    await page.getByLabel('sign-up-form-button').click();
-    await page.getByPlaceholder('Enter your username...').click();
-    await page.getByPlaceholder('Enter your username...').fill('lallalallaa');
-    await page.getByPlaceholder('Enter a description for your').click();
-    await page.getByPlaceholder('Enter a description for your').fill('lalallalaa');
-    await page.getByLabel('validate-details-form-button').click();
-    await page.getByRole('img', { name: 'User profile picture' }).click();
-    await page.getByLabel('Edit Profile').click();
-    await page.getByLabel('delete-user-button').click();
-    code = await page.getByLabel('verify-code').textContent() || "";
-    await page.getByPlaceholder('Enter the code...').click();
-    await page.getByPlaceholder('Enter the code...').fill(code);
-    await page.getByLabel('delete-account-button').click();
-    await expect(page.getByRole('navigation')).toContainText('AREA');
-    await expect(page.locator('.w-80')).toBeVisible();
+    await utilsTestingFunctions.signIn(page);
+    await utilsTestingFunctions.deleteAccount(page);
 });
