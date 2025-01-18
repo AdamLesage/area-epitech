776d684a514f9cdffdea62aee13a229a5f2a1d0d
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2025-01-18 19:13:49 +0100

    add (Aria label) : Added aria label to the back button on client apk page.

--- frontend/src/views/ClientApk.vue
+++ frontend/src/views/ClientApk.vue
@@ -32,7 +32,7 @@
             <p class="text-sm text-home-text-light mb-6 text-center">
                 * Please wait for the download to finish.
             </p>
-            <button @click="goHome" class="btn btn-primary px-12 py-4 text-white bg-home-div rounded-lg transition hover:cursor-pointer z-10">
+            <button aria-label="mobile-app-back-button" @click="goHome" class="btn btn-primary px-12 py-4 text-white bg-home-div rounded-lg transition hover:cursor-pointer z-10">
                 Back
             </button>
         </div>
