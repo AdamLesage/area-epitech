46cecc35b9934046d91bd8799ffb33e3296bbe72
AdamLesage <adamles44@gmail.com>
2025-01-18 18:34:51 +0100

    tests(Playwright tests): add first playwright test to sign up and delete account

--- frontend/e2e/tests/homePageNotLoggedIn.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-    await page.goto('http://area-workspace.fr:8081/#/');
-    await page.waitForTimeout(2000);
-    await page.getByRole('heading', { name: 'Services', exact: true }).click();
-    await page.getByLabel('Explore spotify').click();
-    await expect(page.locator('#app')).toContainText('SPOTIFY');
-});
---
+++ frontend/e2e/tests/userShouldSignUp.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect } from '@playwright/test';
+
+test.use({
+    viewport: {
+        height: 1080,
+        width: 1920
+    }
+});
+
+let code = '';
+
+test('test', async ({ page }) => {
+    await page.goto('http://area-workspace.fr:8081/#/');
+    await page.getByLabel('signup-text-button-router-link').click();
+    await page.getByPlaceholder('Enter your email...').click();
+    await page.getByPlaceholder('Enter your email...').fill('bidule@gmail.com');
+    await page.locator('input[name="password"]').click();
+    await page.locator('input[name="password"]').fill('lalalalallala');
+    await page.locator('input[name="confirmPassword"]').click();
+    await page.locator('input[name="confirmPassword"]').fill('lalalalallala');
+    await page.getByLabel('I agree to the Terms').check();
+    await page.getByLabel('sign-up-form-button').click();
+    await page.getByPlaceholder('Enter your username...').click();
+    await page.getByPlaceholder('Enter your username...').fill('lallalallaa');
+    await page.getByPlaceholder('Enter a description for your').click();
+    await page.getByPlaceholder('Enter a description for your').fill('lalallalaa');
+    await page.getByLabel('validate-details-form-button').click();
+    await page.getByRole('img', { name: 'User profile picture' }).click();
+    await page.getByLabel('Edit Profile').click();
+    await page.getByLabel('delete-user-button').click();
+    code = await page.getByLabel('verify-code').textContent() || "";
+    await page.getByPlaceholder('Enter the code...').click();
+    await page.getByPlaceholder('Enter the code...').fill(code);
+    await page.getByLabel('delete-account-button').click();
+    await expect(page.getByRole('navigation')).toContainText('AREA');
+    await expect(page.locator('.w-80')).toBeVisible();
+});
